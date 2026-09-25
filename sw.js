b9934aa6b6cdd7e2a9b34694abe8c607e45359f3
aglentzakis-dot <aglentzakis@gmail.com>
2026-09-25 23:47:42 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const VERSION='1.5.0';
+const VERSION='1.6.0';
 const C='fakelos-ygeias-'+VERSION;
-const F=['./','index.html','manifest.json','icon-192.png','icon-512.png','apple-touch-icon.png'];
+const F=['./','index.html','i18n.js','manifest.json','icon-192.png','icon-512.png','apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F.map(u=>new Request(u,{cache:'reload'})))).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
